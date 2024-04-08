dfaece85f372a0e2fd3bd19f4b884770543dc0ba
REDJEM Amir <a.redjem@outlook.com>
2024-04-08 21:53:25 +0200

    mise en place des ports

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function App() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(5556);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+
+App();
