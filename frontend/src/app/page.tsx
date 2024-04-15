89495b43259e4509e0cdc7ff2a11179ae9429378
REDJEM Amir <a.redjem@outlook.com>
2024-04-16 00:08:45 +0200

    début de mise en place de plusieur tentative

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,15 @@ import css from "@/styles/page.module.css";
 
 export default function Page() {
   return (
-    <>
-      
-    </>
+    <div className={css.container}>
+      <div className={css.contentPicture}>
+        <img className={css.picture} src="/Bannière.png" alt="La Plateforme_" />
+      </div>
+      <div className={css.contentStart}>
+        <a className={css.link} href="/api/auth/login">
+          <div className={css.text}>Discover</div>
+        </a>
+      </div>
+    </div>
   );
 }
--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -16,6 +16,8 @@ const Board = ({
     const [word, setWord] = useState<string>('');
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string>('');
+    const [attempts, setAttempts] = useState<number>(0);
+    const [validationResult, setValidationResult] = useState<string[]>([]);
 
     useEffect(() => {
         load();
@@ -37,34 +39,73 @@ const Board = ({
     }
 
     const onValidate = () => {
+        const correctGuesses: string[] = [];
+        const misplacedGuesses: string[] = [];
+        const incorrectGuesses: string[] = [];
+        
+        for (let i = 0; i < currentGuess.length; i++) {
+            const guess = currentGuess[i];
+            if (guess === word[i]) {
+                correctGuesses.push(guess);
+            } else if (word.includes(guess)) {
+                misplacedGuesses.push(guess);
+            } else {
+                incorrectGuesses.push(guess);
+            }
+        }
 
+        setValidationResult(correctGuesses.concat(misplacedGuesses).concat(incorrectGuesses));
+
+        if (currentGuess === word) {
+            setAttempts(0);
+            load();
+        } else {
+            setAttempts(prevAttempts => prevAttempts + 1);
+        }
     }
+
 
     const onRemove = () => {
         if (currentGuess.length > 1) {
             const updatedGuess = currentGuess.slice(0, -1);
             setInitialGuess(updatedGuess);
         }
-    }    
+    }
 
     const onRefresh = () => {
         load();
     }
+
+    function getLetterColor(index: number): string {
+        if (index < currentGuess.length) {
+            if (validationResult[index] === currentGuess[index]) {
+                return 'red'; // Lettre correctement placée
+            } else if (validationResult.includes(currentGuess[index])) {
+                return 'yellow'; // Lettre mal placée
+            } else {
+                return 'blue'; // Lettre incorrecte
+            }
+        }
+        return 'transparent'; // Aucune lettre à cet index
+    }
+
 
     if (loading) return <CircularProgress sx={{ color: "rgb(240, 247, 246)" }} />;
     if (error) return <div>Error: {error}</div>;
 
     return (
         <div className={css.container}>
-            <div className={css.containerWord}>
-                {Array.from(word).map((letter, index) => (
-                    <div key={index} className={css.contentCase}>
-                        <div className={css.letter}>
-                            {index < currentGuess.length ? currentGuess[index] : ''}
+            {Array.from({ length: 6 }, (_, rowIndex) => (
+                <div key={rowIndex} className={css.containerWord}>
+                    {Array.from({ length: 6 }, (_, colIndex) => (
+                        <div key={colIndex} className={css.contentCase}>
+                            <div className={css.letter}>
+                                {(rowIndex * 6) + colIndex < currentGuess.length ? currentGuess[(rowIndex * 6) + colIndex] : ''}
+                            </div>
                         </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            ))}
             <div className={css.containerAction}>
                 <div className={css.contentIco} onClick={onValidate}>
                     <FaCheck className={css.ico} />
